1ea1a22fe7c9d7f380a515c06108b6ec4460b39e
Ivan Melnikov <melnikov.ivan@gmail.com>
2014-09-04 23:48:24 +0400

    Basic generation logic

---
+++ tagy.py
@@ -0,0 +1,192 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Generate static portfolio site
+'''
+
+import os
+import shutil
+import yaml
+from collections import defaultdict
+from jinja2 import Environment, PackageLoader
+
+
+# Core config params
+
+CONTENT_DIR = 'content'
+LAYOUT_DIR = 'layout'
+STATIC_DIR = 'static'
+BUILD_DIR = 'public'
+
+
+# Default item params
+
+SITE_PAGES = 'pages'
+
+PAGE_URL = 'url'
+PAGE_NAME = 'name'
+PAGE_PATH = 'path'
+PAGE_LAYOUT = 'layout'
+PAGE_CONTENT = 'content'
+
+
+def generate():
+	site = load_site()
+	generate_site(site)
+
+def load_site():
+	# load basic configs
+	with open('config.yaml', 'r') as f:
+		site = Config(yaml.load(f))
+
+	# add pages
+	site[SITE_PAGES] = load_content()
+
+	# build indexes
+	for page in site.pages:
+		# check all indexes
+		for indexname in iter(site.indexes):
+			index = site.indexes[indexname]
+			terms = getattr(page, indexname, None)
+			if isinstance(terms, list):
+				for term in terms:
+					index.setdefault('terms', {}).setdefault(term, []).append(page)
+
+	return site
+
+def load_content(dir=CONTENT_DIR):
+	pages = []
+	for subdir, dirs, files in os.walk(dir):
+		for file in files:
+			path = os.path.join(subdir, file)
+			pages.append(load_page(path))
+	return pages
+	
+CONFIG = '---\n'
+
+from markdown import markdown
+def load_page(path):
+	with open(path, 'r') as f:
+		content = f.read()
+		i = content.find(CONFIG)
+		page = yaml.load(content[:i]) if i > 0 else {}
+		start = i+len(CONFIG) if i > 0 else 0
+		page[PAGE_CONTENT] = markdown(content[start:].decode('utf-8'))
+		path = path[ : path.index('.')]
+		page[PAGE_NAME] = os.path.basename(path)
+		if path.endswith('/index'): # cut index page
+			path = path[ : path.index('/index')]
+		page[PAGE_PATH] = path[len(CONTENT_DIR + '/') : ]
+		return Config(page)
+
+
+# Generate logic
+
+env = Environment(loader=PackageLoader('portfolio', LAYOUT_DIR))
+
+def generate_site(site):
+	clear()
+
+	for page in site.pages:
+		generate_page(page, site)
+
+	for name in iter(site.indexes):
+		generate_index(name, site)
+
+def clear():
+	# Clear dir
+	folder = BUILD_DIR
+	for name in os.listdir(folder):
+	    path = os.path.join(folder, name)
+	    try:
+	        if os.path.isfile(path):
+	            os.unlink(path)
+	        else: 
+	        	shutil.rmtree(path)
+	    except Exception, e:
+	        print e
+
+	# Copy static dir
+	for file_name in os.listdir(STATIC_DIR):
+	    full_file_name = os.path.join(STATIC_DIR, file_name)
+	    if (os.path.isfile(full_file_name)):
+	        shutil.copy(full_file_name, BUILD_DIR)
+	    else:
+	    	shutil.copytree(full_file_name, os.path.join(BUILD_DIR, file_name))
+
+
+def generate_page(page, site):
+	template = env.get_template(get_template(page))
+	path = get_build_path(page)
+	f = open(path, 'w')
+	# try:
+	html = template.render({'page': page, 'site': site})
+	f.write(html.encode('utf-8'))
+	f.close()
+	# except Exception, e:
+		# print 'Cannot generate page '+page.path, e
+
+def generate_index(name, site):
+	index = getattr(site.indexes, name)
+	for term in iter(index.terms):
+		# TODO: remove layout from yaml
+		template = env.get_template(get_template(index))
+		
+		page = Config()
+		page.path = index.url + '/' + term
+		f = open(get_build_path(page), 'w')
+
+		html = template.render({'term': term, 'pages': index.terms[term], 'site': site})
+		f.write(html.encode('utf-8'))
+		f.close()
+
+
+
+def get_template(page):
+	# TODO: impl inheritance logic
+	if PAGE_LAYOUT in page:
+		return page[PAGE_LAYOUT]
+	return 'index.html'
+
+# Return fancy url for path
+def get_build_path(page):
+	if PAGE_URL in page:
+		return BUILD_DIR + '/' + page.url
+
+	dir = BUILD_DIR + '/' + page.path
+
+	try:
+		os.stat(dir)
+	except:
+		os.makedirs(dir)
+	return dir + '/index.html'
+
+# Jinja filter
+import Image
+def get_thumbnail(value, size=(100, 100)):
+	file_path = BUILD_DIR + value
+	file, ext = os.path.splitext(file_path)
+	path = (file + '-%dx%d' + ext) % size
+	im = Image.open(file_path)
+	im.thumbnail(size, Image.ANTIALIAS)
+	im.save(path, "JPEG")
+	return path[len(BUILD_DIR) : ]
+
+env.filters['thumb'] = get_thumbnail
+
+
+
+# Model for config params
+class Config(dict):
+    def __getattr__(self, attr):
+    	if attr not in self:
+    		return None
+    	result = self[attr]
+    	if isinstance(result, dict):
+    		result = Config(result)
+        return result
+    def __setattr__(self, attr, value):
+        self[attr] = value
+
+generate()
