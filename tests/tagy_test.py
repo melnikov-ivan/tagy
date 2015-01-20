280f2bc40402c03dfbfd480aca5c5db59b82eba8
Ivan Melnikov <melnikov.ivan@gmail.com>
2015-01-20 13:42:07 +0300

    Stub for tests

---
+++ tests/tagy_test.py
@@ -0,0 +1,15 @@
+import unittest
+
+class TagyTest(unittest.TestCase):
+
+	def setUp(self):
+	    print "SETUP!"
+
+	def tearDown(self):
+	    print "TEAR DOWN!"
+
+	def test_basic(self):
+	    print "I RAN!"
+
+if __name__ == '__main__':
+    unittest.main()
