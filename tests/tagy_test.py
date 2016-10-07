7129c6baf8ff5703b082fb149f1b4a5dec23ea82
Ivan Melnikov <melnikov.ivan@gmail.com>
2016-10-07 19:52:43 +0300

    stub for  page test

--- tests/tagy_test.py
+++ tests/tagy_test.py
@@ -1,15 +1,29 @@
 import unittest
+import sys
+sys.path.append('../')
+import tagy
 
 class TagyTest(unittest.TestCase):
 
-	def setUp(self):
-	    print "SETUP!"
+    def setUp(self):
+        print "SETUP!"
 
-	def tearDown(self):
-	    print "TEAR DOWN!"
+    def tearDown(self):
+        print "TEAR DOWN!"
 
-	def test_basic(self):
-	    print "I RAN!"
+    def test_pager(self):
+        
+        page = {tagy.PAGE_LAYOUT:'pager.html', tagy.PAGE_CONTENT:'text', tagy.PAGE_PATH:'pager'}
+        page['list'] = ['one', 'two', 'three']
+        page = tagy.Config(page)
+        site = {}
+
+        # tagy.env = Environment(loader=FileSystemLoader(layout), autoescape=False)
+        tagy.generate_page(page, site)
+
+        if not open('public/pager/index.html').read():
+            return -1
+
 
 if __name__ == '__main__':
     unittest.main()
