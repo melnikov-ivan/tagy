ce72369730263890b839d360f1ffae7c45c02290
Ivan Melnikov <melnikov.ivan@gmail.com>
2021-03-24 13:39:56 +0300

    version 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='tagy',
-    version='0.1.6',
+    version='0.1.7',
     description='Static site generator',
 #    long_description = open("README.md").read(),
     url='https://github.com/melnikov-ivan/tagy',
